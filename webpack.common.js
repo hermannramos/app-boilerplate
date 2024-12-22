ff2c6afbbb42dbbb3f15d0fd1d2aaf8a2717b26d
hermannramos <140882889+hermannramos@users.noreply.github.com>
2024-12-22 09:21:17 +0000

    webpack init

---
+++ webpack.common.js
@@ -0,0 +1,11 @@
+// Importar paquete de rutas absolutas (Ubicación partiendo desde la raiz del proyecto)
+// Rutas relativas para una ubicación partiendo del archivo actual
+const path = require('path');
+
+// module.exports = {
+//     entry:,
+//     output: {
+//         filename: 'bundle,js',
+//         path: path.resolve(__dirname, './dist')
+//     }
+// }
